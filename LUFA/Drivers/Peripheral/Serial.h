14616f27e406b346ac787ec9128ca0c94e92a2bc
Dean <Dean@d5102386-fcda-11dd-9fdb-3debd5008f28>
2010-11-09 09:51:36 +0000

    MultiArchPrep: Add remainder of the peripheral driver architecture dispatch headers.
    git-svn-id: http://lufa-lib.googlecode.com/svn/branches/MultiArchPrep@1569 d5102386-fcda-11dd-9fdb-3debd5008f28

--- LUFA/Drivers/Peripheral/Serial.h
+++ LUFA/Drivers/Peripheral/Serial.h
@@ -51,114 +51,16 @@
 #ifndef __SERIAL_H__
 #define __SERIAL_H__
 
+	/* Macros: */
+	#if !defined(__DOXYGEN__)
+		#define __INCLUDE_FROM_SPI_H
+	#endif
+
 	/* Includes: */
-		#include <avr/io.h>
-		#include <avr/pgmspace.h>
-		#include <stdbool.h>
-
-		#include "../../Common/Common.h"
-		#include "../Misc/TerminalCodes.h"
-
-	/* Enable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			extern "C" {
-		#endif
-
-	/* Public Interface - May be used in end-application: */
-		/* Macros: */
-			/** Macro for calculating the baud value from a given baud rate when the U2X (double speed) bit is
-			 *  not set.
-			 */
-			#define SERIAL_UBBRVAL(baud)    ((((F_CPU / 16) + (baud / 2)) / (baud)) - 1)
-
-			/** Macro for calculating the baud value from a given baud rate when the U2X (double speed) bit is
-			 *  set.
-			 */
-			#define SERIAL_2X_UBBRVAL(baud) ((((F_CPU / 8) + (baud / 2)) / (baud)) - 1)
-
-		/* Function Prototypes: */
-			/** Transmits a given string located in program space (FLASH) through the USART.
-			 *
-			 *  \param[in] FlashStringPtr  Pointer to a string located in program space.
-			 */
-			void Serial_TxString_P(const char* FlashStringPtr) ATTR_NON_NULL_PTR_ARG(1);
-
-			/** Transmits a given string located in SRAM memory through the USART.
-			 *
-			 *  \param[in] StringPtr  Pointer to a string located in SRAM space.
-			 */
-			void Serial_TxString(const char* StringPtr) ATTR_NON_NULL_PTR_ARG(1);
-
-		/* Inline Functions: */
-			/** Initializes the USART, ready for serial data transmission and reception. This initializes the interface to
-			 *  standard 8-bit, no parity, 1 stop bit settings suitable for most applications.
-			 *
-			 *  \param[in] BaudRate     Serial baud rate, in bits per second.
-			 *  \param[in] DoubleSpeed  Enables double speed mode when set, halving the sample time to double the baud rate.
-			 */
-			static inline void Serial_Init(const uint32_t BaudRate,
-			                               const bool DoubleSpeed)
-			{
-				UBRR1  = (DoubleSpeed ? SERIAL_2X_UBBRVAL(BaudRate) : SERIAL_UBBRVAL(BaudRate));
-
-				UCSR1C = ((1 << UCSZ11) | (1 << UCSZ10));
-				UCSR1A = (DoubleSpeed ? (1 << U2X1) : 0);
-				UCSR1B = ((1 << TXEN1)  | (1 << RXEN1));
-
-				DDRD  |= (1 << 3);
-				PORTD |= (1 << 2);
-			}
-
-			/** Turns off the USART driver, disabling and returning used hardware to their default configuration. */
-			static inline void Serial_ShutDown(void)
-			{
-				UCSR1B = 0;
-				UCSR1A = 0;
-				UCSR1C = 0;
-
-				UBRR1  = 0;
-
-				DDRD  &= ~(1 << 3);
-				PORTD &= ~(1 << 2);
-			}
-
-			/** Indicates whether a character has been received through the USART.
-			 *
-			 *  \return Boolean true if a character has been received, false otherwise.
-			 */
-			static inline bool Serial_IsCharReceived(void) ATTR_WARN_UNUSED_RESULT ATTR_ALWAYS_INLINE;
-			static inline bool Serial_IsCharReceived(void)
-			{
-				return ((UCSR1A & (1 << RXC1)) ? true : false);
-			}
-
-			/** Transmits a given byte through the USART.
-			 *
-			 *  \param[in] DataByte  Byte to transmit through the USART.
-			 */
-			static inline void Serial_TxByte(const char DataByte) ATTR_ALWAYS_INLINE;
-			static inline void Serial_TxByte(const char DataByte)
-			{
-				while (!(UCSR1A & (1 << UDRE1)));
-				UDR1 = DataByte;
-			}
-
-			/** Receives a byte from the USART. This function blocks until a byte has been
-			 *  received; if non-blocking behaviour is required, test for a received character
-			 *  beforehand with \ref Serial_IsCharReceived().
-			 *
-			 *  \return Byte received from the USART.
-			 */
-			static inline char Serial_RxByte(void) ATTR_ALWAYS_INLINE;
-			static inline char Serial_RxByte(void)
-			{
-				while (!(UCSR1A & (1 << RXC1)));
-				return UDR1;
-			}
-
-	/* Disable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			}
+		#if (ARCH == ARCH_AVR8)
+			#include "AVR8/Serial.h"
+		#else
+			#error "Serial UART is not available for the currently selected architecture."
 		#endif
 
 #endif
--- LUFA/Drivers/Peripheral/SerialStream.h
+++ LUFA/Drivers/Peripheral/SerialStream.h
@@ -56,58 +56,16 @@
 #ifndef __SERIAL_STREAM_H__
 #define __SERIAL_STREAM_H__
 
-	/* Includes: */
-		#include <avr/io.h>
-		#include <stdio.h>
-
-		#include "Serial.h"
-
-	/* Enable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			extern "C" {
-		#endif
-
-	/* Private Interface - For use in library only: */
+	/* Macros: */
 	#if !defined(__DOXYGEN__)
-		/* External Variables: */
-			extern FILE USARTStream;
-
-		/* Function Prototypes: */
-		#if defined(__INCLUDE_FROM_SERIALSTREAM_C)
-			static int SerialStream_TxByte(char DataByte,
-			                               FILE *Stream) ATTR_NON_NULL_PTR_ARG(2);
-			static int SerialStream_RxByte(FILE *Stream) ATTR_NON_NULL_PTR_ARG(1);
-		#endif
+		#define __INCLUDE_FROM_SPI_H
 	#endif
 
-	/* Public Interface - May be used in end-application: */
-		/* Inline Functions: */
-			/** Initialises the serial stream (and regular USART driver) so that both the stream and regular
-			 *  USART driver functions can be used. Must be called before any stream or regular USART functions.
-			 *
-			 *  \param[in] BaudRate     Baud rate to configure the USART to.
-			 *  \param[in] DoubleSpeed  Enables double speed mode when set, halving the sample time to double the baud rate.
-			 */
-			static inline void SerialStream_Init(const uint32_t BaudRate,
-			                                     const bool DoubleSpeed)
-			{
-				Serial_Init(BaudRate, DoubleSpeed);
-
-				stdout = &USARTStream;
-				stdin  = &USARTStream;
-			}
-
-			/** Turns off the serial stream (and regular USART driver), disabling and returning used hardware to
-			 *  their default configuration.
-			 */
-			static inline void SerialStream_ShutDown(void)
-			{
-				Serial_ShutDown();
-			}
-
-	/* Disable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			}
+	/* Includes: */
+		#if (ARCH == ARCH_AVR8)
+			#include "AVR8/SerialStream.h"
+		#else
+			#error "Serial UART Streams are not available for the currently selected architecture."
 		#endif
 
 #endif
