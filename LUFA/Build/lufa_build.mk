afe51e83cba985d66f38e89828a5984a933688fd
Paul Chavent <paul.chavent@fnac.net>
2014-07-20 18:19:48 +0200

    Fix clean target.
    Remove the lib.a since the build rule of .a append a lib prefix.
    Allow to override the clean target : this way we can remove added lst files for instance.

--- LUFA/Build/lufa_build.mk
+++ LUFA/Build/lufa_build.mk
@@ -253,9 +253,9 @@ mostlyclean:
 	rm -f $(DEPENDENCY_FILES)
 
 # Cleans all build files, leaving only the original source code
-clean: mostlyclean
+clean:: mostlyclean
 	@echo $(MSG_REMOVE_CMD) Removing output files of \"$(TARGET)\"
-	rm -f $(TARGET).elf $(TARGET).hex $(TARGET).bin $(TARGET).eep $(TARGET).map $(TARGET).lss $(TARGET).sym $(TARGET).a
+	rm -f $(TARGET).elf $(TARGET).hex $(TARGET).bin $(TARGET).eep $(TARGET).map $(TARGET).lss $(TARGET).sym lib$(TARGET).a
 
 # Performs a complete build of the user application and prints size information afterwards
 all: build_begin elf hex bin lss sym size build_end
