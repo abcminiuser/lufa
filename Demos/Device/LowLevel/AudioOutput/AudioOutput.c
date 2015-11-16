b234071c222e3a8035b594c992b4a9d553eb0a7f
Ag Primatic <agprimatic@gmail.com>
2015-11-16 16:25:57 -0500

    Added return code to EVENT_USB_Device_ControlRequest()

--- Demos/Device/LowLevel/AudioOutput/AudioOutput.c
+++ Demos/Device/LowLevel/AudioOutput/AudioOutput.c
@@ -156,7 +156,7 @@ void EVENT_USB_Device_ConfigurationChanged(void)
  *  the device from the USB host before passing along unhandled control requests to the library for processing
  *  internally.
  */
-void EVENT_USB_Device_ControlRequest(void)
+int EVENT_USB_Device_ControlRequest(void)
 {
 	/* Process General and Audio specific control requests */
 	switch (USB_ControlRequest.bRequest)
@@ -170,6 +170,7 @@ void EVENT_USB_Device_ControlRequest(void)
 
 				/* Check if the host is enabling the audio interface (setting AlternateSetting to 1) */
 				StreamingAudioInterfaceSelected = ((USB_ControlRequest.wValue) != 0);
+				return 1;
 			}
 
 			break;
@@ -181,6 +182,7 @@ void EVENT_USB_Device_ControlRequest(void)
 			{
 				Endpoint_ClearSETUP();
 				Endpoint_ClearStatusStage();
+				return 1;
 			}
 
 			break;
@@ -205,6 +207,7 @@ void EVENT_USB_Device_ControlRequest(void)
 
 					/* Adjust sample reload timer to the new frequency */
 					OCR0A = ((F_CPU / 8 / CurrentAudioSampleFrequency) - 1);
+					return 1;
 				}
 			}
 
@@ -229,11 +232,13 @@ void EVENT_USB_Device_ControlRequest(void)
 					Endpoint_ClearSETUP();
 					Endpoint_Write_Control_Stream_LE(SampleRate, sizeof(SampleRate));
 					Endpoint_ClearOUT();
+					return 1;
 				}
 			}
 
 			break;
 	}
+	return 0;
 }
 
 /** ISR to handle the reloading of the PWM timer with the next sample. */
