3a50c9ea821c406226b9d32424e151ce9d951ecc
Benjamin Riggs <deisum@gmail.com>
2019-03-05 21:37:17 -0600

    Set unique PID.

--- Demos/Device/LowLevel/WebUSB/Descriptors.c
+++ Demos/Device/LowLevel/WebUSB/Descriptors.c
@@ -54,7 +54,7 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
 	.Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
 
 	.VendorID               = 0x03EB,
-	.ProductID              = 0x204F,
+	.ProductID              = 0x206F,
 	.ReleaseNumber          = VERSION_BCD(0,0,1),
 
 	.ManufacturerStrIndex   = STRING_ID_Manufacturer,
