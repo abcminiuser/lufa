0a8c79f1db8a40a5287d16809103695bd5efabdd
MasterQ <masterq@mutemail.de>
2021-12-06 00:54:45 +0100

    Latest changes for testing

--- Projects/AVRISP-MKII/Lib/XPROG/XPROGTarget.c
+++ Projects/AVRISP-MKII/Lib/XPROG/XPROGTarget.c
@@ -266,15 +266,10 @@ void XPROGTarget_SendIdle(void)
 		while (PIND & (1 << 5));
 #elif (ARCH == ARCH_XMEGA)
 		/* As the pin inversion is active we need to wait inverted */
-// 		uint32_t timeout = 10000;
 		while (!(PDI_PORT.IN & PDI_XCK_MASK))
 			;
-		
-// 		timeout = 10000;
 		while ((PDI_PORT.IN & PDI_XCK_MASK))
 			;
-		
-// 		timeout = 10000;
 		while (!(PDI_PORT.IN & PDI_XCK_MASK))
 			;
 #endif
@@ -296,21 +291,12 @@ static void XPROGTarget_SetTxMode(void)
 	UCSR1B |=  (1 << TXEN1);
 #elif (ARCH == ARCH_XMEGA)
 	/* As the pin is actived, we need to wait inverted */
-// 	uint32_t timeout = 10000;
 	while (!(PDI_PORT.IN & PDI_XCK_MASK))
-	{
-// 		timeout--;
-	}
-// 	timeout = 10000;
+		;
 	while ((PDI_PORT.IN & PDI_XCK_MASK))
-	{
-// 		timeout--;
-	}
-// 	timeout = 10000;
+		;
 	while (!(PDI_PORT.IN & PDI_XCK_MASK))
-	{
-// 		timeout--;
-	}
+		;
 
 	PDI_PORT.OUTSET = PDI_TX_MASK;
 	PDI_PORT.DIRSET = PDI_TX_MASK;
