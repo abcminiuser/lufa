ed5ba7da538c87ffb386eac0cb7dcb651c514bd3
MasterQ <masterq@mutemail.de>
2021-12-23 14:00:02 +0100

    Updated configuration for to newly implented functionality and updated Values. Added ascii art schematic for default pinout.

--- Projects/AVRISP-MKII/Config/AppConfig.h
+++ Projects/AVRISP-MKII/Config/AppConfig.h
@@ -54,19 +54,71 @@
 		#define VTARGET_ADC_CHANNEL        2
 		#define VTARGET_REF_VOLTS          5
 		#define VTARGET_SCALE_FACTOR       1
-//		#define VTARGET_USE_INTERNAL_REF
+		#define VTARGET_USE_INTERNAL_REF
 	#elif (ARCH == ARCH_XMEGA)
-		#define SPI_REG                 SPIC
-		#define SPI_PORT                PORTC
+/* Wiring in default config (REMAP SPI Enable or USART SPI Master)
+ * PORTC
+ * PIN
+ *  0 O---120Ohm---|
+ *                 O-- RESET (PDI/ISP PIN5)
+ *  1 O---120Ohm---|
+ * 
+ *  2 O---180Ohm---O
+ *                 |
+ *  3 O---180Ohm---O
+ *                 |
+ *  4 x            O-- MISO/DAT (PDI/ISP PIN1)
+ *                 | 
+ *  5 O---120Ohm---|-- SCK (PDI/ISP PIN5)
+ *                 |
+ *  6 O---120Ohm---O
+ * 
+ *  7 O---120Ohm------ MOSI (PDI/ISP PIN4)
+ * 
+ * 
+ *  GND O---------------O--------- GND (PDI/ISP PIN6)
+ *                      |
+ *  PA0 O----O-1.5KOhm--O
+ *           |                      Scale theoritical 7.66667 with this Resistors, but we needed to adjust to 8.43333.
+ *           O--10KOhm--O           You can read voltage from target
+ *                      ?           Or you can supply the target. We prefer to have addional Power OUT Pins for supply the target.
+ *  VCC O---------???---O---------- VCC (PDI/ISP PIN2) (Dangerous! But can be helpfull, you can also leave it open or use other options)
+ * 
+ * This allows to program PDI/ISP but only 3.3V!!!!!!
+ */ 
+		//Remaping spi will swap SCK and MOSI (Same pinout as USART in SPI master mode)
+		#define REMAP_SPI
+		
+		//Use USART interface in SPI master mode instead of the SPI module.
+		//Recommended it allows higher precision of the SPI CLK.
+		#define USART_SPI_MASTER
 
-		#define SPI_SCK_MASK           (1 << 7)
-		#define SPI_SCK_CTRL            PIN7CTRL
+		#ifdef USART_SPI_MASTER
+			#define USART_SPI_REG            USARTC1
+		#else
+			#define SPI_REG                  SPIC
+		#endif
 
-		#define SPI_MISO_MASK          (1 << 6)
-		#define SPI_MISO_CTRL           PIN6CTRL
+		#define SPI_PORT                   PORTC
 
-		#define SPI_MOSI_MASK          (1 << 5)
-		#define SPI_MOSI_CTRL           PIN5CTRL
+		#if defined(REMAP_SPI) || defined(USART_SPI_MASTER)
+			#define SPI_SCK_MASK            (1 << 5)
+			#define SPI_SCK_CTRL             PIN5CTRL
+
+			#define SPI_MOSI_MASK           (1 << 7)
+			#define SPI_MOSI_CTRL            PIN7CTRL
+		#else
+			#define SPI_SCK_MASK            (1 << 7)
+			#define SPI_SCK_CTRL             PIN7CTRL
+
+			#define SPI_MOSI_MASK           (1 << 5)
+			#define SPI_MOSI_CTRL            PIN5CTRL
+		#endif
+
+		#define SPI_MISO_MASK             (1 << 6)
+		#define SPI_MISO_CTRL              PIN6CTRL
+
+
 
 		#define RESCUE_PORT             PORTD
 		#define RESCUE_TIMER            TCD0
@@ -94,18 +146,19 @@
 		#define DELAY_TIMER_OVF_vect       TCC0_OVF_vect
 		#define SW_SPI_TIMER               TCC1
 		#define SW_SPI_TIMER_CCA_vect      TCC1_CCA_vect
+		#define SW_SPI_TIMER_CCB_vect      TCC1_CCB_vect
 		#define SW_SPI_TIMER_OVF_vect      TCC1_OVF_vect
 		#define SW_SPI_PIN_IRQ_vect        PORTC_INT0_vect
-		#define VTARGET_ADC_CHANNEL        2 //Unimplemented
-		#define VTARGET_REF_VOLTS          5 //Unimplemented
-		#define VTARGET_SCALE_FACTOR       1 //Unimplemented
+		#define VTARGET_ADC_CHANNEL        1 //Hardcoded allways 1 Connected to PA1
+		#define VTARGET_REF_VOLTS          1.00 //Hardcoded allways 1.00
+		#define VTARGET_SCALE_FACTOR       8.43333 //GND<->R1<-ADC->R2<->SIGNAL | SCALE = (R1+R2)/R1
 	#endif
 	#define ENABLE_ISP_PROTOCOL
 	#define ENABLE_XPROG_PROTOCOL
 
 
 
-	#define NO_VTARGET_DETECT
+// 	#define NO_VTARGET_DETECT
 //	#define XCK_RESCUE_CLOCK_ENABLE
 //	#define INVERTED_ISP_MISO
 
